2104e52949cff7c14aee1274a75cafff0ec7268c
root <root@DESKTOP-ESKH5GM.localdomain>
2019-11-17 17:43:35 +0200

    update main

--- main.c
+++ main.c
@@ -11,8 +11,8 @@ int main()
 	scanf("%f", &n);
 	int n_i = (int)n;
 	float x = sub(add(Exp(n_i),Pow(n, 3)),2);
-	double y = add(mul(n,3),mul(Pow(n, 2),2));
-	double z =sub(Div(mul(Pow(n, 3),4),5),mul(n,2));
+	float y = add(mul(n,3),mul(Pow(n, 2),2));
+	float z =sub(Div(mul(Pow(n, 3),4),5),mul(n,2));
 	printf("Result 1: %.4f\n", x);
 	printf("Result 2: %.4f\n", y);
 	printf("Result 3: %.4f\n", z);
